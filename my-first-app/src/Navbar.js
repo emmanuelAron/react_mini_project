9051cf74a5c6312c89a94e23e4486eb1426fc3a4
Aron <emmanuel.aron@gmail.com>
2024-02-05 17:36:17 +0100

    First commit

---
+++ my-first-app/src/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Logo from "./assets/logo.png";
+
+function Navbar(){
+    return(
+        <>
+        <nav ><img className="logo" src="{logo} "></img>
+        <h1>{name}</h1>
+        </nav>
+        </>
+    )
+}
+export default Navbar;
+/* function Navbar () {
+
+  return (
+
+    <nav>
+
+      <p>React - Components & props</p>
+
+    </nav>
+
+  );
+
+}
+
+ 
+
+export default Navbar; */
